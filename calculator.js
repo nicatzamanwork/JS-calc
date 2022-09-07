30f5e69cba10e75a622076cac05499c9e5628e39
nicatzamanwork <87694171+nicatzamanwork@users.noreply.github.com>
2022-09-07 11:57:01 +0400

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,22 @@
+let result=document.getElementById('inputext');
+
+let calculate=(number)=>{
+    result.value+=number;
+}
+
+let Result=()=>{
+    try{
+        result.value=eval(result.value)
+    }
+    catch(err){
+        alert("Enter valid  result");
+    }
+
+}
+
+function clr(){
+    result.value=" ";
+}
+function del(){
+    result.value=result.value.slice(0,-1);
+}
